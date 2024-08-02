665924b09a45eb963598ff2ca0aacf4b658fbbc2
Tumsa <t.letif@student.tue.nl>
2024-08-02 09:12:50 +0200

    button click counter, event handlers

--- part_1/part1_c/src/main.jsx
+++ part_1/part1_c/src/main.jsx
@@ -1,5 +1,11 @@
-import ReactDOM from 'react-dom/client'
+export default App
 
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+// Ensure this block is executed only once
+const rootElement = document.getElementById('root')
+if (!rootElement.hasChildNodes()) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(<App />)
+}
